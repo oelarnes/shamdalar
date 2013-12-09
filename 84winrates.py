4bb27a071261ef4b891e789f402bddeababd2543
Joel Barnes <oelarnes@gmail.com>
2013-12-09 12:09:41 -0800

    fancier graphs in 84winrates.py

--- 84winrates.py
+++ 84winrates.py
@@ -5,11 +5,12 @@ import matplotlib.pyplot as plt
 def elo_winrate(diff): return 1-1/(10**(float(diff)/400.0)+1)
 
 MY_RATING = 1769
-N = 100
-AVE_RATING = 1670
+N = 48
+AVE_RATING = 1680
 SIGMA = 60
+BREADTH = 4
 
-def make_p0(n,mu,sigma, breadth=4):
+def make_p0(n,mu,sigma, breadth):
     ratings = [mu]
     for i in range(1,n/2):
         ratings.append(mu + 2*breadth*sigma*i/n) 
@@ -20,15 +21,17 @@ def make_p0(n,mu,sigma, breadth=4):
     p_0 = [p/total for p in p_temp]
     return ratings,p_0
 
-ratings,p_0 = make_p0(N,AVE_RATING,SIGMA)
+ratings,p_0 = make_p0(N,AVE_RATING,SIGMA,BREADTH)
 
 if sum(p_0) > 1.0001 or sum(p_0) < 0.9999:
     print "NONNORMALIZED DIST"
 if len(ratings)!=len(p_0): print 'INVALID DIST'
 
-def winrate(probs) : return sum([probs[i]*elo_winrate(MY_RATING - ratings[i]) for i in range(len(ratings))])
+def winrate(probs) : return sum([probs[i]*elo_winrate(MY_RATING - ratings[i])
+                                 for i in range(len(ratings))])
 
-def next_round(probs): return [2*probs[i]*sum([probs[j]*elo_winrate(ratings[i]-ratings[j]) for j in range(len(ratings))])
+def next_round(probs): return [2*probs[i]*sum([probs[j]*elo_winrate(
+                    ratings[i]-ratings[j]) for j in range(len(ratings))])
                      for i in range(len(ratings))]
 
 def n_rounds(p, n):
@@ -37,15 +40,19 @@ def n_rounds(p, n):
     return p
 
 p = p_0
-w = []
-for n in range(3):
-    plt.plot(ratings,p)
-    p = n_rounds(p,1)
-    w.append(winrate(p))
 
-for item in w:
-    print item
-    
+plt.ylabel('proportion of ' + str(SIGMA/2) + ' point interval')
+plt.xlabel('Elo rating')
+
+for n in range(3):
+    k=1
+    plt.plot(ratings,[el*N/(4*BREADTH) for el in p],
+             label = 'Rd '+str(k*n+1)+' wr = ' + str("{0:.3f}".format(
+                                                     round(winrate(p),3))))
+    p = n_rounds(p,k)
+
+
+plt.legend(loc=2)
 plt.show()
 
 
